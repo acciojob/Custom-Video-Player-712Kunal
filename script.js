40677f4d7df805401c64c01bf20d5255eb951ecf
Kunal Jadhav <155985737+712Kunal@users.noreply.github.com>
2026-09-18 02:19:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -36,9 +36,11 @@ function updateButton() {
 
 // Update progress bar
 function handleProgress() {
+  if (!video.duration) return;
+
   const percent = (video.currentTime / video.duration) * 100;
 
-  progressBar.style.flexBasis = `${percent}%`;
+  progressBar.style.width = `${percent}%`;
 }
 
 
